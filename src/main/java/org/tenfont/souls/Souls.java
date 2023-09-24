046e617d86453b4c429a5d8cf6d6a98154de8258
TenFont <thetenfont@gmail.com>
2023-09-24 17:05:39 +0500

    What

--- src/main/java/org/tenfont/souls/Souls.java
+++ src/main/java/org/tenfont/souls/Souls.java
@@ -1,13 +1,50 @@
 package org.tenfont.souls;
 
+import net.citizensnpcs.api.CitizensAPI;
+import net.citizensnpcs.api.npc.NPC;
+import net.citizensnpcs.trait.SkinTrait;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.title.Title;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public final class Souls extends JavaPlugin {
-
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getCommand("spawn").setExecutor((sender, command, label, args) -> {
+            if (!(sender instanceof Player player)) return true;
+            Location spawnLocation = player.getLocation();
+            spawnLocation.setPitch(0);
+            Bukkit.getScheduler().runTaskLater(this, () -> {
+                player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 220, 1));
+                player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 220, 1));
+                player.showTitle(Title.title(
+                        Component.text("Careful...").color(TextColor.fromHexString("#c42b2b")),
+                        Component.text("There are souls nearby").color(TextColor.fromHexString("#9c9c9c")).decorate(TextDecoration.ITALIC)
+                ));
 
+                NPC npc = CitizensAPI.getNPCRegistry().createNPC(EntityType.PLAYER, "&7Lost Soul");
+                SkinTrait skinTrait = npc.getOrAddTrait(SkinTrait.class);
+                skinTrait.setSkinName("sunalsun", true);
+                npc.setProtected(false);
+                npc.spawn(spawnLocation);
+                Player playerEntity = (Player) npc.getEntity();
+                playerEntity.setNoDamageTicks(0);
+                playerEntity.setWalkSpeed(0.5f);
+                npc.getNavigator().setTarget(player, true);
+                Bukkit.getScheduler().runTaskLater(this, () -> {
+                    npc.destroy();
+                }, 200);
+            }, 100);
+            return true;
+        });
     }
 
     @Override
--- src/main/java/org/tenfont/souls/util/SkinUtils.java
+++ src/main/java/org/tenfont/souls/util/SkinUtils.java
@@ -1,2 +1,16 @@
-package org.tenfont.souls.util;public class SkinUtils {
+package org.tenfont.souls.util;
+
+import java.util.Random;
+import java.util.UUID;
+
+public class SkinUtils {
+    private static final UUID[] lostSoulSkins = {
+            UUID.fromString("65a289dd-de9a-4933-b7af-cb325085cad5"),
+            UUID.fromString("f587e7d1-08ef-4c43-90cb-37e237000153")
+    };
+
+    public static UUID getRandomLostSoulSkin() {
+        int index = new Random().nextInt(lostSoulSkins.length);
+        return lostSoulSkins[index];
+    }
 }
